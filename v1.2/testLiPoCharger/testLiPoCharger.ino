1c3d5bb9535a9eab0370a22f4407588ca7b433c5
jdpeiffer6 <jdp6n8@mail.missouri.edu>
2020-06-04 11:45:18 -0500

    Added datasheets

--- v1.2/testLiPoCharger/testLiPoCharger.ino
+++ v1.2/testLiPoCharger/testLiPoCharger.ino
@@ -1,6 +1,4 @@
-int sensorValue;
 int STS;
-float BatVoltage;
 
 #define STSpin A4   //status output from LiPo charger. Should read low until charging done
 #define BatPin A0 
@@ -9,19 +7,12 @@ void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   Serial1.begin(9600);
   pinMode(STSpin,INPUT);
-  pinMode(BatPin,INPUT);
-  pinMode(9,OUTPUT);
   Serial1.println("BEGIN");
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(9,HIGH);
-  sensorValue=analogRead(BatPin);
-  BatVoltage=(5.0/1023.0)*sensorValue;
-  STS=analogRead(STSpin);
-  Serial1.print("Battery read: ");
-  Serial1.println(sensorValue);
+  STS=digitalRead(STSpin);
   Serial1.print("STS read: ");
   Serial1.println(STS);
   Serial1.println("");
